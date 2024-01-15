73ab5b8214636f9a157b1253ba35bf1a493c06d3
Derrick Hammer <derrick@derrickhammer.com>
2024-01-15 12:02:15 -0500

    refactor: add a means of saving the config if any default value is not set

--- config/config.go
+++ config/config.go
@@ -26,8 +26,6 @@ func Init() error {
 	viper.SetEnvPrefix("LUME_WEB_PORTAL")
 	viper.AutomaticEnv()
 
-	defaults()
-
 	err := viper.ReadInConfig()
 	if err != nil {
 		if errors.As(err, &viper.ConfigFileNotFoundError{}) {
@@ -36,17 +34,45 @@ func Init() error {
 			if err != nil {
 				return err
 			}
-			return nil
+			return writeDefaults()
 		}
 		return err
+	}
+
+	return writeDefaults()
+}
+
+func writeDefaults() error {
+	defaults := map[string]interface{}{
+		"core.post-upload-limit":                  1024 * 1024 * 1000,
+		"core.log.level":                          "info",
+		"protocol.s5.p2p.maxOutgoingPeerFailures": 10,
+		"protocol.s5.p2p.network":                 "",
+	}
+
+	changes := false
+
+	for key, value := range defaults {
+		if writeDefault(key, value) {
+			changes = true
+		}
+	}
+
+	if changes {
+		err := viper.WriteConfig()
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func defaults() {
-	viper.SetDefault("core.post-upload-limit", 1024*1024*1000)
-	viper.SetDefault("core.log.level", "info")
+func writeDefault(key string, value interface{}) bool {
+	if !viper.IsSet(key) {
+		viper.SetDefault(key, value)
+		return true
+	}
 
-	viper.SetDefault("protocol.s5.p2p.maxOutgoingPeerFailures", 10)
+	return false
 }
