e4626fdbb8da29c5bab2fe909849fc1d5688f67e
Derrick Hammer <derrick@derrickhammer.com>
2024-01-15 14:06:16 -0500

    fix: wrong api endpoint

--- storage/storage.go
+++ storage/storage.go
@@ -47,7 +47,7 @@ func (s StorageServiceImpl) PutFile(file io.ReadSeeker, bucket string, generateP
 		SetFormData(map[string]string{
 			"bucket": bucket,
 		}).
-		SetBody(buf).Put("/api/worker/{path}")
+		SetBody(buf).Put("/api/worker/object/{path}")
 	if err != nil {
 		return nil, err
 	}
