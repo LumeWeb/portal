26c28db1f22dd457114ceee10c953bf054f564a6
Derrick Hammer <derrick@derrickhammer.com>
2024-01-19 17:05:18 -0500

    feat: create ApplyMiddlewares helper

--- api/api.go
+++ api/api.go
@@ -3,7 +3,10 @@ package api
 import (
 	"git.lumeweb.com/LumeWeb/portal/interfaces"
 	"github.com/julienschmidt/httprouter"
+	"go.sia.tech/jape"
 )
+
+type MiddlewareFunc func(jape.Handler) jape.Handler
 
 func Init(router interfaces.APIRegistry) error {
 	router.Register("s5", NewS5())
@@ -16,4 +19,11 @@ func registerProtocolSubdomain(portal interfaces.Portal, mux *httprouter.Router,
 	domain := portal.Config().GetString("core.domain")
 
 	(*router)[name+"."+domain] = mux
+}
+func ApplyMiddlewares(handler jape.Handler, middlewares ...MiddlewareFunc) jape.Handler {
+	// Apply each middleware in reverse order
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		handler = middlewares[i](handler)
+	}
+	return handler
 }
