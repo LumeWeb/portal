ae4901757bf0dd5b84814f914b0d5b791df75357
Derrick Hammer <derrick@derrickhammer.com>
2024-01-21 00:08:49 -0500

    fix: log PutFile error

--- storage/storage.go
+++ storage/storage.go
@@ -509,6 +509,11 @@ func (s *StorageServiceImpl) buildNewTusUploadTask(upload *models.TusUpload) (jo
 
 			err = s.PutFile(reader, s.portal.Config().GetString("core.storage.s3.bucket"), dbHash)
 
+			if err != nil {
+				s.portal.Logger().Error("Could not upload file", zap.Error(err))
+				return err
+			}
+
 			return nil
 		}, upload)
 
