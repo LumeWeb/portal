dd857650e07b6f622b3a7636fb08962be46f1dcf
Derrick Hammer <derrick@derrickhammer.com>
2024-01-21 01:41:04 -0500

    fix: need create a compound index on hash and deleted at

--- db/models/tus_upload.go
+++ db/models/tus_upload.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type TusUpload struct {
 	gorm.Model
-	Hash       string `gorm:"uniqueIndex"`
+	Hash       string `gorm:"uniqueIndex:idx_hash_deleted"`
 	UploadID   string `gorm:"uniqueIndex"`
 	UploaderID uint
 	UploaderIP string
 	Uploader   User `gorm:"foreignKey:UploaderID"`
 	Protocol   string
+	DeletedAt  gorm.DeletedAt `gorm:"uniqueIndex:idx_hash_deleted"`
 }
