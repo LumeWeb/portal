9f6f2c9c875de2ee5108dc9743b2b30dcb42cd13
Derrick Hammer <derrick@derrickhammer.com>
2024-02-13 20:58:17 -0500

    feat: add purpose to jwt with consts

--- account/jwt.go
+++ account/jwt.go
@@ -6,16 +6,24 @@ import (
 	"time"
 )
 
-func GenerateToken(domain string, privateKey ed25519.PrivateKey, userID uint) (string, error) {
-	return GenerateTokenWithDuration(domain, privateKey, userID, time.Hour*24)
+type JWTPurpose string
+
+const (
+	JWTPurposeLogin JWTPurpose = "login"
+)
+
+func GenerateToken(domain string, privateKey ed25519.PrivateKey, userID uint, purpose JWTPurpose) (string, error) {
+	return GenerateTokenWithDuration(domain, privateKey, userID, time.Hour*24, purpose)
 }
-func GenerateTokenWithDuration(domain string, privateKey ed25519.PrivateKey, userID uint, duration time.Duration) (string, error) {
+
+func GenerateTokenWithDuration(domain string, privateKey ed25519.PrivateKey, userID uint, duration time.Duration, purpose JWTPurpose) (string, error) {
 	// Define the claims
 	claims := jwt.MapClaims{
 		"iss": domain,
 		"sub": userID,
 		"exp": time.Now().Add(duration).Unix(),
 		"iat": time.Now().Unix(),
+		"aud": string(purpose),
 	}
 
 	// Create the token
