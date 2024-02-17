3ded11d7052cf79843efda61c73edc84a9567ffd
Derrick Hammer <derrick@derrickhammer.com>
2024-02-17 05:38:50 -0500

    fix: HttpMiddlewareFunc needs its own case

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -51,8 +51,11 @@ func ApplyMiddlewares(handler jape.Handler, middlewares ...interface{}) jape.Han
 		case JapeMiddlewareFunc:
 			mid := middlewares[i].(JapeMiddlewareFunc)
 			handler = mid(handler)
-		case func(http.Handler) http.Handler, HttpMiddlewareFunc:
+		case func(http.Handler) http.Handler:
 			mid := middlewares[i].(func(http.Handler) http.Handler)
+			handler = AdaptMiddleware(mid)(handler)
+		case HttpMiddlewareFunc:
+			mid := middlewares[i].(HttpMiddlewareFunc)
 			handler = AdaptMiddleware(mid)(handler)
 
 		default:
