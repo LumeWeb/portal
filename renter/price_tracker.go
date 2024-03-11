96ec3b85010b0b63949858e211917b6e8e923f81
Derrick Hammer <derrick@derrickhammer.com>
2024-03-11 08:01:56 -0400

    fix: dont compute by rate the max rpc price

--- renter/price_tracker.go
+++ renter/price_tracker.go
@@ -151,9 +151,9 @@ SELECT AVG(rate) as average_rate FROM (
 		return err
 	}
 
-	maxRPCPrice, err := computeByRate(p.config.Config().Core.Storage.Sia.MaxRPCSCPrice, averageRate, "max rpc price")
-	if err != nil {
-		return err
+	maxRPCPrice, ok := new(big.Rat).SetString(p.config.Config().Core.Storage.Sia.MaxRPCSCPrice)
+	if !ok {
+		return errors.New("failed to parse max rpc price")
 	}
 
 	maxRPCPrice = ratDivide(maxRPCPrice, 1_000_000)
