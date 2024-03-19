9ea77fb5c3477be199ab1e04d461f2a0ae19596b
Derrick Hammer <derrick@derrickhammer.com>
2024-03-19 09:29:29 -0400

    fix: check to ensure error is something other than ErrRecordNotFound, or that exists is true

--- account/account.go
+++ account/account.go
@@ -280,11 +280,7 @@ func (s AccountServiceDefault) UpdateAccountName(userId uint, firstName string, 
 
 func (s AccountServiceDefault) UpdateAccountEmail(userId uint, email string, password string) error {
 	exists, _, err := s.EmailExists(email)
-	if err != nil {
-		return err
-	}
-
-	if exists {
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) || exists {
 		return NewAccountError(ErrKeyEmailAlreadyExists, nil)
 	}
 
