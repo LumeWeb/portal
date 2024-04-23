c508b522ea6321ae0ff2e37a4c6b6e7f88a64742
Derrick Hammer <derrick@derrickhammer.com>
2024-04-23 04:11:29 -0400

    refactor(protocol): use central registry to track protocol instances, but init it based on the fx DI collections

--- protocols/protocols.go
+++ protocols/protocols.go
@@ -13,7 +13,7 @@ import (
 func BuildProtocols(cm *config.Manager) fx.Option {
 	var options []fx.Option
 	enabledProtocols := cm.Viper().GetStringSlice("core.protocols")
-	for _, entry := range registry.GetRegistry() {
+	for _, entry := range registry.GetEntryRegistry() {
 		if lo.Contains(enabledProtocols, entry.Key) {
 			options = append(options, entry.Module)
 			if entry.PreInitFunc != nil {
@@ -28,6 +28,10 @@ func BuildProtocols(cm *config.Manager) fx.Option {
 	}
 
 	options = append(options, fx.Invoke(func(params initParams) error {
+		for _, protocol := range params.Protocols {
+			registry.RegisterProtocol(protocol)
+		}
+
 		for _, protocol := range params.Protocols {
 			err := cm.ConfigureProtocol(protocol.Name(), protocol.Config())
 			if err != nil {
@@ -46,26 +50,16 @@ func BuildProtocols(cm *config.Manager) fx.Option {
 	return fx.Module("protocols", fx.Options(options...))
 }
 
-type LifecyclesParams struct {
-	fx.In
-
-	Protocols []registry.Protocol `group:"protocol"`
-}
-
-func SetupLifecycles(lifecycle fx.Lifecycle, params LifecyclesParams) error {
-	for _, entry := range registry.GetRegistry() {
-		for _, protocol := range params.Protocols {
-			if protocol.Name() == entry.Key {
-				lifecycle.Append(fx.Hook{
-					OnStart: func(ctx context.Context) error {
-						return protocol.Start(ctx)
-					},
-					OnStop: func(ctx context.Context) error {
-						return protocol.Stop(ctx)
-					},
-				})
-			}
-		}
+func SetupLifecycles(lifecycle fx.Lifecycle) error {
+	for _, proto := range registry.GetAllProtocols() {
+		lifecycle.Append(fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				return proto.Start(ctx)
+			},
+			OnStop: func(ctx context.Context) error {
+				return proto.Stop(ctx)
+			},
+		})
 	}
 
 	return nil
