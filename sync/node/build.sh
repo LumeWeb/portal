0824dca3e78adddcb532c34d042c9d4c1ca915bf
Derrick Hammer <derrick@derrickhammer.com>
2024-05-03 08:08:57 -0400

    fix(sync): handle dynamic go path lookup for grpc_stdio.proto

--- sync/node/build.sh
+++ sync/node/build.sh
@@ -5,5 +5,22 @@ cp -r ./node_modules/grpc-health-check/proto/health ./app/proto
 mkdir -p app/app/app/build/Release && cp ./node_modules/sodium-native/prebuilds/linux-x64/sodium-native.node app/app/app/build/Release
 mkdir -p src/generated
 ./node_modules/protobufjs-cli/bin/pbjs -t json ../proto/protocol.proto > src/generated/protobuf.json
-./node_modules/protobufjs-cli/bin/pbjs -t json ../../vendor/github.com/hashicorp/go-plugin/internal/plugin/grpc_stdio.proto > src/generated/grpc_stdio.json
+
+
+# Check if vendor exists
+if [ -d "../../vendor/github.com/hashicorp" ]; then
+    proto_file="$(readlink -f ../../vendor)/github.com/hashicorp/go-plugin/internal/plugin/grpc_stdio.proto"
+# Check if GOPATH is set
+elif [ -d "$GOPATH/go/pkg/mod" ]; then
+    proto_file=$(find "$GOPATH/go/pkg/mod/github.com/hashicorp" -name grpc_stdio.proto -print -quit)
+# Check if $HOME/go/pkg/mod exists
+elif [ -d "$HOME/go/pkg/mod/github.com/hashicorp" ]; then
+    proto_file="$(find "$HOME/go/pkg/mod/github.com/hashicorp" -name grpc_stdio.proto -print -quit)"
+else
+    echo "Error: Could not find the parent directory of github.com/hashicorp"
+    exit 1
+fi
+
+# Run protobufjs-cli command
+./node_modules/protobufjs-cli/bin/pbjs -t json "$proto_file" > src/generated/grpc_stdio.json
 ./node_modules/.bin/rollup -c rollup.config.js --silent
