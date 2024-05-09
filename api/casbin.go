84b6131ac4c81efa90e6ff785222702a53e38cc6
Derrick Hammer <derrick@derrickhammer.com>
2024-05-09 18:02:02 -0400

    refactor(api): have casbin use go embed to load the model and policy and get rid of the custom policy implementation. rewrite the admin policy.

--- api/casbin.go
+++ api/casbin.go
@@ -1,23 +1,26 @@
 package api
 
 import (
+	"embed"
+	_ "embed"
+	"os"
+
 	"github.com/casbin/casbin/v2"
-	"github.com/casbin/casbin/v2/model"
-	"github.com/casbin/casbin/v2/persist"
+	casbinfsadapter "github.com/naucon/casbin-fs-adapter"
 	"go.uber.org/zap"
-	"strings"
-	"sync"
 )
 
-func NewCasbin(logger *zap.Logger) *casbin.Enforcer {
-	m := model.NewModel()
-	m.AddDef("r", "r", "sub, obj, act")
-	m.AddDef("p", "p", "sub, obj, act")
-	m.AddDef("g", "g", "_, _")
-	m.AddDef("e", "e", "some(where (p.eft == allow))")
-	m.AddDef("m", "m", "g(r.sub, p.sub) && r.obj == p.obj && r.act == p.act")
+//go:embed casbin/*
+var casbinData embed.FS
 
-	a := NewPolicyAdapter(logger)
+func NewCasbin(logger *zap.Logger) (*casbin.Enforcer, error) {
+	fsys := os.DirFS("casbin")
+	a := casbinfsadapter.NewAdapter(fsys, "policy.csv")
+	m, err := casbinfsadapter.NewModel(fsys, "model.conf")
+
+	if err != nil {
+		return nil, err
+	}
 
 	e, err := casbin.NewEnforcer(m, a)
 	if err != nil {
@@ -32,69 +35,5 @@ func NewCasbin(logger *zap.Logger) *casbin.Enforcer {
 		logger.Fatal("Failed to load policies into Casbin model", zap.Error(err))
 	}
 
-	return e
-}
-
-type PolicyAdapter struct {
-	policy []string
-	lock   sync.RWMutex
-	logger *zap.Logger
-}
-
-// NewPolicyAdapter creates a new PolicyAdapter instance.
-func NewPolicyAdapter(logger *zap.Logger) *PolicyAdapter {
-	return &PolicyAdapter{
-		policy: make([]string, 0),
-		logger: logger,
-	}
-}
-
-// LoadPolicy loads all policy rules from the storage.
-func (a *PolicyAdapter) LoadPolicy(model model.Model) error {
-	a.lock.RLock()
-	defer a.lock.RUnlock()
-
-	for _, line := range a.policy {
-		err := persist.LoadPolicyLine(line, model)
-		if err != nil {
-			a.logger.Fatal("Failed to load policy line", zap.Error(err))
-		}
-	}
-	return nil
-}
-
-// SavePolicy saves all policy rules to the storage.
-func (a *PolicyAdapter) SavePolicy(model model.Model) error {
-	return nil
-}
-
-// AddPolicy adds a policy rule to the storage.
-// AddPolicy adds a policy rule to the storage.
-func (a *PolicyAdapter) AddPolicy(sec string, ptype string, rule []string) error {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-
-	// Create a line representing the policy rule with the section
-	line := sec + ", " + ptype + ", " + strings.Join(rule, ", ")
-
-	// Check if the policy rule already exists
-	for _, existingLine := range a.policy {
-		if line == existingLine {
-			return nil // Policy rule already exists, no need to add it again
-		}
-	}
-
-	// Add the policy rule to the storage
-	a.policy = append(a.policy, line)
-	return nil
-}
-
-// RemovePolicy removes a policy rule from the storage.
-func (a *PolicyAdapter) RemovePolicy(sec string, ptype string, rule []string) error {
-	return nil
-}
-
-// RemoveFilteredPolicy removes policy rules that match the filter from the storage.
-func (a *PolicyAdapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error {
-	return nil
+	return e, nil
 }
