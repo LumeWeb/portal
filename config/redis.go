e10473cde314f815090c5a0aeb922e0188b5d4d9
Derrick Hammer <derrick@derrickhammer.com>
2024-05-10 09:49:11 -0400

    feat(config): add Client to redis config for a shared client

--- config/redis.go
+++ config/redis.go
@@ -1,6 +1,10 @@
 package config
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/redis/go-redis/v9"
+)
 
 var _ Validator = (*RedisConfig)(nil)
 var _ Defaults = (*RedisConfig)(nil)
@@ -9,6 +13,7 @@ type RedisConfig struct {
 	Address  string `mapstructure:"address"`
 	Password string `mapstructure:"password"`
 	DB       int    `mapstructure:"db"`
+	client   *redis.Client
 }
 
 func (r *RedisConfig) Defaults() map[string]interface{} {
@@ -23,4 +28,15 @@ func (r *RedisConfig) Validate() error {
 		return errors.New("address is required")
 	}
 	return nil
+}
+
+func (r *RedisConfig) Client() (*redis.Client, error) {
+	if r.client == nil {
+		r.client = redis.NewClient(&redis.Options{
+			Addr:     r.Address,
+			Password: r.Password,
+		})
+	}
+
+	return r.client, nil
 }
