7b4cfbc96733477a3e99ca0fc2b3852fe1992d03
Derrick Hammer <derrick@derrickhammer.com>
2024-05-10 06:53:05 -0400

    feat(config): add Client method to EtcdConfig to have a persistent shared client instance

--- config/etcd.go
+++ config/etcd.go
@@ -1,12 +1,18 @@
 package config
 
-import "errors"
+import (
+	"errors"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
 
 var _ Defaults = (*EtcdConfig)(nil)
 
 type EtcdConfig struct {
 	Endpoints   []string `mapstructure:"endpoints"`
 	DialTimeout int      `mapstructure:"dial_timeout"`
+	client      *clientv3.Client
 }
 
 func (r *EtcdConfig) Validate() error {
@@ -20,4 +26,20 @@ func (r *EtcdConfig) Defaults() map[string]interface{} {
 	return map[string]interface{}{
 		"dial_timeout": 5,
 	}
+}
+
+func (r *EtcdConfig) Client() (*clientv3.Client, error) {
+	if r.client == nil {
+		client, err := clientv3.New(clientv3.Config{
+			Endpoints:   r.Endpoints,
+			DialTimeout: time.Duration(r.DialTimeout) * time.Second,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		r.client = client
+	}
+
+	return r.client, nil
 }
