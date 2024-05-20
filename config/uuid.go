2c079dfbed20c473e49f87f30435a0d107415513
Derrick Hammer <derrick@derrickhammer.com>
2024-05-20 11:41:49 -0400

    feat(config): add UnmarshalYAML to uuid

--- config/uuid.go
+++ config/uuid.go
@@ -8,6 +8,7 @@ import (
 type UUID uuid.UUID
 
 var _ yaml.Marshaler = (*UUID)(nil)
+var _ yaml.Unmarshaler = (*UUID)(nil)
 
 func NewUUID() UUID {
 	return UUID(uuid.New())
@@ -15,6 +16,17 @@ func NewUUID() UUID {
 
 func (u UUID) MarshalYAML() (interface{}, error) {
 	return uuid.UUID(u).String(), nil
+}
+
+func (u UUID) UnmarshalYAML(value *yaml.Node) error {
+	id, err := uuid.Parse(value.Value)
+	if err != nil {
+		return err
+	}
+
+	u = UUID(id)
+	return nil
+
 }
 
 func (u UUID) Bytes() []byte {
