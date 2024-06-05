96ace0414ceda0621c0989db5270cdeee466f423
Derrick Hammer <derrick@derrickhammer.com>
2024-06-04 23:25:32 -0400

    refactor(core): create a portalcmd package and make cmd/portal a basic proxy to portalcmd.Main in the style of caddy

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package portalcmd
+
+import (
+	"github.com/LumeWeb/portal"
+	"github.com/LumeWeb/portal/config"
+	"github.com/LumeWeb/portal/core"
+	"go.uber.org/zap"
+	"os"
+)
+
+func Main() {
+	cfg, err := config.NewManager()
+	logger := core.NewLogger(cfg)
+	if err != nil {
+		logger.Fatal("Failed to load config", zap.Error(err))
+	}
+
+	err = cfg.Init()
+	if err != nil {
+		logger.Fatal("Failed to initialize config", zap.Error(err))
+	}
+
+	logger.SetLevelFromConfig()
+
+	portal.NewActivePortal(core.NewBaseContext(cfg, logger))
+
+	err = portal.Init()
+
+	if err != nil {
+		logger.Fatal("Failed to initialize portal", zap.Error(err))
+		os.Exit(exitCodeFailedStartup)
+	}
+
+	err = portal.Start()
+
+	if err != nil {
+		logger.Error("Failed to start portal", zap.Error(err))
+		os.Exit(exitCodeFailedStartup)
+	}
+
+	trapSignals()
+
+	err = portal.Serve()
+	if err != nil {
+		logger.Error("Failed to serve portal", zap.Error(err))
+		os.Exit(exitCodeFailedStartup)
+	}
+}
