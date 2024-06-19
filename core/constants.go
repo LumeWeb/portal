4f80aa481bea566a85fd0c968d5aa919390dc203
Derrick Hammer <derrick@derrickhammer.com>
2024-06-18 21:53:05 -0400

    refactor: change subdomain handling on user service to use new event system and add a const in core with the event name

--- core/constants.go
+++ core/constants.go
@@ -2,6 +2,8 @@ package core
 
 import "github.com/docker/go-units"
 
-const PROOF_EXTENSION = ".obao"
-const S3_MULTIPART_MAX_PARTS = 9500
-const S3_MULTIPART_MIN_PART_SIZE = uint64(5 * units.MiB)
+const (
+	PROOF_EXTENSION            = ".obao"
+	S3_MULTIPART_MAX_PARTS     = 9500
+	S3_MULTIPART_MIN_PART_SIZE = uint64(5 * units.MiB)
+)
