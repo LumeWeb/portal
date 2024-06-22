fb4bfb9af6c86845835b7707309ef9cb194d700c
Derrick Hammer <derrick@derrickhammer.com>
2024-06-21 23:31:53 -0400

    refactor: move events to a dedicated package outside of core

---
+++ event/storage.go
@@ -0,0 +1,23 @@
+package event
+
+import "go.lumeweb.com/portal/core"
+
+const (
+	EVENT_STORAGE_OBJECT_UPLOADED = "storage.object.uploaded"
+)
+
+func init() {
+	core.RegisterEvent(EVENT_STORAGE_OBJECT_UPLOADED, &StorageObjectUploadedEvent{})
+}
+
+type StorageObjectUploadedEvent struct {
+	core.Event
+}
+
+func (e StorageObjectUploadedEvent) SetObjectMetadata(metadata *core.UploadMetadata) {
+	e.Set("metadata", metadata)
+}
+
+func (e UserSubdomainSetEvent) ObjectMetadata() *core.UploadMetadata {
+	return e.Get("metadata").(*core.UploadMetadata)
+}
---
+++ service/event.go
@@ -0,0 +1,5 @@
+package service
+
+import (
+	_ "go.lumeweb.com/portal/event"
+)
