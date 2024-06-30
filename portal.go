f4bbd0291194efa05ec3796d508b25e9603b98b2
Derrick Hammer <derrick@derrickhammer.com>
2024-06-30 04:38:08 -0400

    refactor(portal): split up logic in init, start, and stop to private methods per component/step

--- portal.go
+++ portal.go
@@ -6,6 +6,7 @@ import (
 	"go.lumeweb.com/portal/core"
 	"go.lumeweb.com/portal/db"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 	"os"
 	"reflect"
 	"sync"
@@ -31,158 +32,42 @@ type PortalImpl struct {
 func (p *PortalImpl) Init() error {
 	ctx := p.Context()
 
+	ctx.Logger().Info("Initializing portal")
+
 	dbInst, ctxOpts := db.NewDatabase(ctx)
 
-	svcs := core.GetServices()
+	ctxOpts, err := p.initServices(&ctx)
 
-	for _, svcInfo := range svcs {
-		svc, opts, err := svcInfo.Factory()
-		if err != nil {
-			ctx.Logger().Error("Error creating service", zap.String("service", svcInfo.ID), zap.Error(err))
-			return err
-		}
-
-		if opts != nil {
-			ctxOpts = append(ctxOpts, opts...)
-		}
-
-		ctxOpts = append(ctxOpts, core.ContextWithService(svcInfo.ID, svc))
-
-		if !core.IsCoreService(svcInfo.ID) {
-			if configurableSvc, ok := svc.(core.Configurable); ok {
-				cfg, err := configurableSvc.Config()
-				if err != nil {
-					ctx.Logger().Error("Error getting service config", zap.String("service", svcInfo.ID), zap.Error(err))
-					return err
-				}
-
-				svcConfig, ok := cfg.(config.ServiceConfig)
-				if !ok {
-					ctx.Logger().Error(config.ErrInvalidServiceConfig.Error(), zap.String("service", svcInfo.ID))
-					return config.ErrInvalidServiceConfig
-				}
-				plugin := core.GetPluginForService(svcInfo.ID)
-				if plugin == "" {
-					ctx.Logger().Error("Error getting plugin for service", zap.String("service", svcInfo.ID))
-					continue
-				}
-				if err := ctx.Config().ConfigureService(plugin, svcInfo.ID, svcConfig); err != nil {
-					ctx.Logger().Error("Error configuring service", zap.String("service", svcInfo.ID), zap.Error(err))
-					return err
-				}
-			}
-		}
+	if err != nil {
+		return err
 	}
 
-	plugins := core.GetPlugins()
+	ctxOpts, err = p.initModels(&ctx, dbInst, ctxOpts)
 
-	for _, plugin := range plugins {
-		if plugin.Models != nil && len(plugin.Models) > 0 {
-			for _, model := range plugin.Models {
-				typ := reflect.TypeOf(model)
-				if typ.Kind() != reflect.Ptr {
-					ctx.Logger().Error("Model must be a pointer", zap.String("model", typ.Name()))
-					return core.ErrInvalidModel
-				}
-
-				ctxOpts = append(ctxOpts, core.ContextWithStartupFunc(func(ctx core.Context) error {
-					if err := dbInst.AutoMigrate(model); err != nil {
-						ctx.Logger().Error("Error migrating model", zap.String("model", typ.Name()), zap.Error(err))
-						return err
-					}
-
-					return nil
-				}))
-			}
-		}
+	if err != nil {
+		return err
 	}
 
-	for _, plugin := range plugins {
-		if core.PluginHasProtocol(plugin) {
-			_proto, opts, err := plugin.Protocol()
-			if err != nil {
-				ctx.Logger().Error("Error building protocol", zap.String("plugin", plugin.ID), zap.Error(err))
-				return err
-			}
+	ctxOpts, err = p.initProtocols(&ctx)
 
-			if _proto == nil {
-				continue
-			}
-
-			ctxOpts = append(ctxOpts, opts...)
-
-			core.RegisterProtocol(plugin.ID, _proto)
-		}
+	if err != nil {
+		return err
 	}
 
-	for _, plugin := range plugins {
-		if core.PluginHasAPI(plugin) {
-			api, opts, err := plugin.API()
-			if err != nil {
-				ctx.Logger().Error("Error building API", zap.String("plugin", plugin.ID), zap.Error(err))
-				return err
-			}
+	ctxOpts, err = p.initAPIs(&ctx)
 
-			if api == nil {
-				continue
-			}
-
-			ctxOpts = append(ctxOpts, opts...)
-			core.RegisterAPI(plugin.ID, api)
-		}
+	if err != nil {
+		return err
 	}
 
-	for _, plugin := range plugins {
-		for _, e := range plugin.Events {
-			core.RegisterEvent(e.Name(), e)
-		}
-	}
+	ctxOpts = p.initCron(ctxOpts)
 
-	for name, _proto := range core.GetProtocols() {
-		err := ctx.Config().ConfigureProtocol(name, _proto.Config())
-		if err != nil {
-			ctx.Logger().Error("Error configuring protocol", zap.String("protocol", _proto.Name()), zap.Error(err))
-			return err
-		}
-
-		if initProto, ok := _proto.(core.ProtocolInit); ok {
-			if err := initProto.Init(&ctx); err != nil {
-				ctx.Logger().Error("Error initializing protocol", zap.String("protocol", _proto.Name()), zap.Error(err))
-				return err
-			}
-		}
-	}
-
-	for name, api := range core.GetAPIs() {
-		err := ctx.Config().ConfigureAPI(name, api.Config())
-		if err != nil {
-			ctx.Logger().Error("Error configuring api", zap.String("api", api.Subdomain()), zap.Error(err))
-			return err
-		}
-		if initApi, ok := api.(core.APIInit); ok {
-			opts, err := initApi.Init()
-			if err != nil {
-				ctx.Logger().Error("Error initializing api", zap.String("api", api.Subdomain()), zap.Error(err))
-				return err
-			}
-
-			ctxOpts = append(ctxOpts, opts...)
-		}
-	}
-
-	for _, plugin := range plugins {
-		if core.PluginHasCron(plugin) {
-			cronFactory := plugin.Cron()
-			if cronFactory == nil {
-				continue
-			}
-
-			ctxOpts = append(ctxOpts, core.ContextWithCron(cronFactory))
-		}
+	if err := p.initEvents(); err != nil {
+		return err
 	}
 
 	ctxOpts = append(ctxOpts, core.ContextWithEvents(core.GetEvents()...))
-	ctx, err := core.NewContext(ctx.Config(), ctx.Logger(), ctxOpts...)
+	ctx, err = core.NewContext(ctx.Config(), ctx.Logger(), ctxOpts...)
 
 	if err != nil {
 		ctx.Logger().Error("Error creating context", zap.Error(err))
@@ -198,6 +83,222 @@ func (p *PortalImpl) Start() error {
 	ctx := p.Context()
 	ctx.Logger().Info("Starting portal")
 
+	if err := p.startStartupFuncs(ctx); err != nil {
+		return err
+	}
+
+	if err := p.startProtocols(ctx); err != nil {
+		return err
+	}
+
+	if err := p.startCron(ctx); err != nil {
+		return err
+	}
+
+	if err := p.startHTTP(ctx); err != nil {
+		return err
+	}
+
+	if err := p.startMailer(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PortalImpl) Stop() error {
+	ctx := p.Context()
+	ctx.Logger().Info("Stopping portal")
+
+	if err := p.stopProtocols(ctx); err != nil {
+		return err
+	}
+
+	if err := p.runExitFuncs(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PortalImpl) Serve() error {
+	ctx := p.Context()
+	ctx.Logger().Info("Serving portal")
+
+	httpSvc := ctx.Service(core.HTTP_SERVICE)
+
+	if httpSvc == nil {
+		ctx.Logger().Error("HTTP service not found")
+		return errors.New("http service not found")
+	}
+
+	return httpSvc.(core.HTTPService).Serve()
+}
+
+func (p *PortalImpl) initServices(ctx *core.Context) (ctxOpts []core.ContextBuilderOption, err error) {
+	svcs := core.GetServices()
+
+	for _, svcInfo := range svcs {
+		svc, opts, err := svcInfo.Factory()
+		if err != nil {
+			ctx.Logger().Error("Error creating service", zap.String("service", svcInfo.ID), zap.Error(err))
+			return nil, err
+		}
+
+		if opts != nil {
+			ctxOpts = append(ctxOpts, opts...)
+		}
+
+		ctxOpts = append(ctxOpts, core.ContextWithService(svcInfo.ID, svc))
+
+		if !core.IsCoreService(svcInfo.ID) {
+			if configurableSvc, ok := svc.(core.Configurable); ok {
+				cfg, err := configurableSvc.Config()
+				if err != nil {
+					ctx.Logger().Error("Error getting service config", zap.String("service", svcInfo.ID), zap.Error(err))
+					return nil, err
+				}
+
+				svcConfig, ok := cfg.(config.ServiceConfig)
+				if !ok {
+					ctx.Logger().Error(config.ErrInvalidServiceConfig.Error(), zap.String("service", svcInfo.ID))
+					return nil, config.ErrInvalidServiceConfig
+				}
+				plugin := core.GetPluginForService(svcInfo.ID)
+				if plugin == "" {
+					ctx.Logger().Error("Error getting plugin for service", zap.String("service", svcInfo.ID))
+					continue
+				}
+				if err := ctx.Config().ConfigureService(plugin, svcInfo.ID, svcConfig); err != nil {
+					ctx.Logger().Error("Error configuring service", zap.String("service", svcInfo.ID), zap.Error(err))
+					return nil, err
+				}
+			}
+		}
+
+		return nil, nil
+	}
+
+	return ctxOpts, nil
+}
+
+func (p *PortalImpl) initModels(ctx *core.Context, dbInst *gorm.DB, ctxOpts []core.ContextBuilderOption) ([]core.ContextBuilderOption, error) {
+	plugins := core.GetPlugins()
+
+	for _, plugin := range plugins {
+		if plugin.Models != nil && len(plugin.Models) > 0 {
+			for _, model := range plugin.Models {
+				typ := reflect.TypeOf(model)
+				if typ.Kind() != reflect.Ptr {
+					ctx.Logger().Error("Model must be a pointer", zap.String("model", typ.Name()))
+					return nil, core.ErrInvalidModel
+				}
+
+				ctxOpts = append(ctxOpts, core.ContextWithStartupFunc(func(ctx core.Context) error {
+					if err := dbInst.AutoMigrate(model); err != nil {
+						ctx.Logger().Error("Error migrating model", zap.String("model", typ.Name()), zap.Error(err))
+						return err
+					}
+
+					return nil
+				}))
+			}
+		}
+	}
+
+	return ctxOpts, nil
+}
+
+func (p *PortalImpl) initProtocols(ctx *core.Context) (ctxOpts []core.ContextBuilderOption, err error) {
+	for name, _proto := range core.GetProtocols() {
+		err := ctx.Config().ConfigureProtocol(name, _proto.Config())
+		if err != nil {
+			ctx.Logger().Error("Error configuring protocol", zap.String("protocol", _proto.Name()), zap.Error(err))
+			return nil, err
+		}
+
+		if initProto, ok := _proto.(core.ProtocolInit); ok {
+			if err := initProto.Init(ctx); err != nil {
+				ctx.Logger().Error("Error initializing protocol", zap.String("protocol", _proto.Name()), zap.Error(err))
+				return nil, err
+			}
+		}
+	}
+
+	return ctxOpts, nil
+}
+
+func (p *PortalImpl) initAPIs(ctx *core.Context) (ctxOpts []core.ContextBuilderOption, err error) {
+	for name, api := range core.GetAPIs() {
+		err := ctx.Config().ConfigureAPI(name, api.Config())
+		if err != nil {
+			ctx.Logger().Error("Error configuring api", zap.String("api", api.Subdomain()), zap.Error(err))
+			return nil, err
+		}
+		if initApi, ok := api.(core.APIInit); ok {
+			opts, err := initApi.Init()
+			if err != nil {
+				ctx.Logger().Error("Error initializing api", zap.String("api", api.Subdomain()), zap.Error(err))
+				return nil, err
+			}
+
+			ctxOpts = append(ctxOpts, opts...)
+		}
+	}
+
+	return ctxOpts, nil
+}
+
+func (p *PortalImpl) initEvents() error {
+	for _, plugin := range core.GetPlugins() {
+		for _, e := range plugin.Events {
+			core.RegisterEvent(e.Name(), e)
+		}
+	}
+
+	return nil
+}
+
+func (p *PortalImpl) configureProtocols(ctx *core.Context) error {
+	for name, _proto := range core.GetProtocols() {
+		err := ctx.Config().ConfigureProtocol(name, _proto.Config())
+		if err != nil {
+			ctx.Logger().Error("Error configuring protocol", zap.String("protocol", _proto.Name()), zap.Error(err))
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *PortalImpl) configureAPIs(ctx *core.Context) error {
+	for name, api := range core.GetAPIs() {
+		err := ctx.Config().ConfigureAPI(name, api.Config())
+		if err != nil {
+			ctx.Logger().Error("Error configuring api", zap.String("api", api.Subdomain()), zap.Error(err))
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *PortalImpl) initCron(ctxOpts []core.ContextBuilderOption) []core.ContextBuilderOption {
+	for _, plugin := range core.GetPlugins() {
+		if core.PluginHasCron(plugin) {
+			cronFactory := plugin.Cron()
+			if cronFactory == nil {
+				continue
+			}
+
+			ctxOpts = append(ctxOpts, core.ContextWithCron(cronFactory))
+		}
+	}
+
+	return ctxOpts
+}
+
+func (p *PortalImpl) startStartupFuncs(ctx core.Context) error {
 	for _, startupFunc := range ctx.StartupFuncs() {
 		if err := startupFunc(ctx); err != nil {
 			ctx.Logger().Error("Error starting portal", zap.Error(err))
@@ -205,6 +306,10 @@ func (p *PortalImpl) Start() error {
 		}
 	}
 
+	return nil
+}
+
+func (p *PortalImpl) startProtocols(ctx core.Context) error {
 	for _, proto := range core.GetProtocols() {
 		if startPlugin, ok := proto.(core.ProtocolStart); ok {
 			if err := startPlugin.Start(ctx); err != nil {
@@ -214,6 +319,10 @@ func (p *PortalImpl) Start() error {
 		}
 	}
 
+	return nil
+}
+
+func (p *PortalImpl) startCron(ctx core.Context) error {
 	cronSvc := ctx.Service(core.CRON_SERVICE)
 
 	if cronSvc == nil {
@@ -226,6 +335,10 @@ func (p *PortalImpl) Start() error {
 		return err
 	}
 
+	return nil
+}
+
+func (p *PortalImpl) startHTTP(ctx core.Context) error {
 	httpSvc := ctx.Service(core.HTTP_SERVICE)
 
 	if httpSvc == nil {
@@ -233,12 +346,16 @@ func (p *PortalImpl) Start() error {
 		return errors.New("http service not found")
 	}
 
-	err = httpSvc.(core.HTTPService).Init()
+	err := httpSvc.(core.HTTPService).Init()
 
 	if err != nil {
 		return err
 	}
 
+	return nil
+}
+
+func (p *PortalImpl) startMailer(ctx core.Context) error {
 	mailerSvc := ctx.Service(core.MAILER_SERVICE)
 
 	if mailerSvc == nil {
@@ -261,10 +378,7 @@ func (p *PortalImpl) Start() error {
 
 	return nil
 }
-
-func (p *PortalImpl) Stop() error {
-	ctx := p.Context()
-	ctx.Logger().Info("Stopping portal")
+func (p *PortalImpl) stopProtocols(ctx core.Context) error {
 	for _, proto := range core.GetProtocols() {
 		if stopPlugin, ok := proto.(core.ProtocolStop); ok {
 			if err := stopPlugin.Stop(ctx); err != nil {
@@ -274,6 +388,10 @@ func (p *PortalImpl) Stop() error {
 		}
 	}
 
+	return nil
+}
+
+func (p *PortalImpl) runExitFuncs(ctx core.Context) error {
 	for _, exitFunc := range ctx.ExitFuncs() {
 		if err := exitFunc(ctx); err != nil {
 			ctx.Logger().Error("Error stopping portal", zap.Error(err))
@@ -281,20 +399,6 @@ func (p *PortalImpl) Stop() error {
 	}
 
 	return nil
-}
-
-func (p *PortalImpl) Serve() error {
-	ctx := p.Context()
-	ctx.Logger().Info("Serving portal")
-
-	httpSvc := ctx.Service(core.HTTP_SERVICE)
-
-	if httpSvc == nil {
-		ctx.Logger().Error("HTTP service not found")
-		return errors.New("http service not found")
-	}
-
-	return httpSvc.(core.HTTPService).Serve()
 }
 
 func NewPortal(ctx core.Context) *PortalImpl {
