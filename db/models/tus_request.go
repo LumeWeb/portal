0b46c4e973304be559c2fdfc1cac5c6e62102734
Derrick Hammer <derrick@derrickhammer.com>
2024-10-31 06:03:55 -0400

    fix(tus): wrong index name

--- db/models/tus_request.go
+++ db/models/tus_request.go
@@ -8,7 +8,7 @@ func init() {
 
 type TUSRequest struct {
 	gorm.Model
-	RequestID   uint `gorm:"uniqueIndex:idx_ipfs_req_deleted_at_request_id"`
+	RequestID   uint `gorm:"uniqueIndex:idx_tus_requests_request_id"`
 	Request     Request
 	TUSUploadID string `gorm:"type:varchar(500);uniqueIndex"`
 	Completed   bool
