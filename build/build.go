0ec893b2c35b359af9a51cefcc2d9e120a8052e8
Derrick Hammer <derrick@derrickhammer.com>
2024-11-05 16:25:24 -0500

    refactor: externalize platform detection
    - Add GoVersion, Platform and Architecture fields to build vars
    - Move runtime detection out of Info methods into explicit initialization
    - Simplify Info() method implementation to return struct directly

--- build/build.go
+++ build/build.go
@@ -1,8 +1,11 @@
 package build
 
 var (
-	Version   string
-	GitCommit string
-	GitBranch string
-	BuildTime string
+	Version      string
+	GitCommit    string
+	GitBranch    string
+	BuildTime    string
+	GoVersion    string
+	Platform     string
+	Architecture string
 )
