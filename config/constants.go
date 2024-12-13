eeb81f5ae13118989d252adb50d1a03faabb2af4
Derrick Hammer <derrick@derrickhammer.com>
2024-12-13 16:18:13 -0500

    refactor(config): create dedicated constants file and add ENV_PREFIX

---
+++ config/constants.go
@@ -0,0 +1,40 @@
+package config
+
+const (
+	//
+	ENV_PREFIX = "PORTAL_"
+
+	// Configuration keys and flags
+	CLUSTER_CONFIG_KEY = "config"
+	FLAG_SYNC          = "sync"
+	FLAG_VOLATILE      = "volatile"
+
+	// File extensions and names
+	CONFIG_EXTENSION  = ".yaml"
+	CoreConfigFile    = "core" + CONFIG_EXTENSION
+	SectionConfigFile = "default" + CONFIG_EXTENSION
+
+	// Directory names
+	PluginsDir = "plugins.d"
+	ProtoDir   = "proto.d"
+	ServiceDir = "service.d"
+	APIDir     = "api.d"
+
+	// Section specifiers
+	protoSectionSpecifier   = "plugin.%s.protocol"
+	apiSectionSpecifier     = "plugin.%s.api"
+	serviceSectionSpecifier = "plugin.%s.service.%s"
+
+	// Struct tags
+	mapStructureTag = "config"
+)
+
+var (
+	// Default config directory paths
+	configDirPaths = []string{
+		"/etc/lumeweb/portal",
+		"$HOME/.lumeweb/portal",
+		"./portal.yaml",
+		"./",
+	}
+)
