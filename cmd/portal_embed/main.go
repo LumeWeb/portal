812d82761834ad3ff3622e935b0c0596e52748d3
Derrick Hammer <derrick@derrickhammer.com>
2024-12-13 21:08:20 -0500

    refactor(cli): add portal_embed pkg

---
+++ cmd/portal_embed/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"go.lumeweb.com/portal/cmd/internal/cli"
+	_ "go.lumeweb.com/portal/service"
+	"os"
+)
+
+func main() {
+	command := cli.NewPortalCLI()
+	ctx := context.Background()
+	if err := command.Run(ctx, os.Args); err != nil {
+		os.Exit(1)
+	}
+}
