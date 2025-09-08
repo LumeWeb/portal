0d3d2d67bcc54c6ab82bab88df1ad38b699a1c82
Derrick Hammer <derrick@derrickhammer.com>
2025-09-08 08:28:44 -0400

    fix: standardize on BootCompleted naming in event system

--- event/boot.go
+++ event/boot.go
@@ -13,21 +13,22 @@ type BootStartEvent struct {
 	Context core.Context
 }
 
+type BootCompletedEvent struct {
+	Context core.Context
+}
+
+func NewBootCompletedEvent(ctx core.Context) *BootCompletedEvent {
+	return &BootCompletedEvent{
+		Context: ctx,
+	}
+}
+
 func NewBootStartEvent(ctx core.Context) *BootStartEvent {
 	return &BootStartEvent{
 		Context: ctx,
 	}
 }
 
-type BootCompleteEvent struct {
-	Context core.Context
-}
-
-func NewBootCompleteEvent(ctx core.Context) *BootCompleteEvent {
-	return &BootCompleteEvent{
-		Context: ctx,
-	}
-}
 
 // OnBootStart registers a handler to run when the system boot starts.
 // This is a convenience wrapper around Listen for the EVENT_BOOT_START event.
@@ -37,10 +38,10 @@ func OnBootStart(ctx core.Context, handler func(core.Context) error, priority ..
 	}, priority...)
 }
 
-// OnBootComplete registers a handler to run when the system boot completes.
+// OnBootCompleted registers a handler to run when the system boot completes.
 // This is a convenience wrapper around Listen for the EVENT_BOOT_COMPLETED event.
-func OnBootComplete(ctx core.Context, handler func(core.Context) error, priority ...int) {
-	core.Listen[BootCompleteEvent](ctx, EVENT_BOOT_COMPLETED, func(e *core.CoreEvent[BootCompleteEvent]) error {
+func OnBootCompleted(ctx core.Context, handler func(core.Context) error, priority ...int) {
+	core.Listen[BootCompletedEvent](ctx, EVENT_BOOT_COMPLETED, func(e *core.CoreEvent[BootCompletedEvent]) error {
 		return handler(e.Data.Context)
 	}, priority...)
 }
